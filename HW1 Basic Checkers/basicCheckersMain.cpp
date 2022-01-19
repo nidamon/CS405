05b22cddaad831d0eea8f5d40eda8f1c3bc7d99f
Nathan Damon <nidamon@alaska.edu>
2022-01-18 17:07:51 -0900

    Added SFML to the project and put the "SFML works!" test code into main().

--- HW1 Basic Checkers/basicCheckersMain.cpp
+++ HW1 Basic Checkers/basicCheckersMain.cpp
@@ -5,7 +5,27 @@ CS 405
 This is the file that contains main and will run the program
 */
 
+#include<SFML/Graphics.hpp>
 
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
 
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 
+	return 0;
+}
